e4b89240a543ada1eff140516d9a5048ef9a4f5d
nimkoes <xxxelppa@gmail.com>
2023-03-08 23:10:38 +0900

    환경설정중

---
+++ src/main/java/me/nimkoes/ltoplz/LtoplzApplication.java
@@ -0,0 +1,13 @@
+package me.nimkoes.ltoplz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtoplzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtoplzApplication.class, args);
+    }
+
+}
