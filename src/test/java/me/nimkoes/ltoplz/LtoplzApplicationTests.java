6e4bb2d8fac9537f45d523a7ca5c0a251a2773f1
nimkoes <xxxelppa@gmail.com>
2023-03-15 12:49:29 +0900

    코드 정리

--- src/test/java/me/nimkoes/ltoplz/LtoplzApplicationTests.java
+++ src/test/java/me/nimkoes/ltoplz/LtoplzApplicationTests.java
@@ -1,49 +1,13 @@
 package me.nimkoes.ltoplz;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import me.nimkoes.ltoplz.entity.DrwMasterEntity;
-import me.nimkoes.ltoplz.model.DrwModel;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.time.LocalDate;
-
-import static me.nimkoes.ltoplz.entity.QDrwMasterEntity.drwMasterEntity;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
 @ActiveProfiles("local")
 class LtoplzApplicationTests {
-    @PersistenceContext
-    EntityManager em;
-    @Autowired
-    private JPAQueryFactory queryFactory;
 
-    @BeforeEach
-    public void before() {
-        queryFactory = new JPAQueryFactory(em);
-
-        DrwModel drwModel = new DrwModel(
-                1L, LocalDate.now(),
-                12, 21, 34, 3, 5, 6, 27);
-
-        em.persist(DrwMasterEntity.byDrwModel(drwModel));
-    }
-
-    @Test
-    public void test() {
-        DrwMasterEntity result = queryFactory
-                .selectFrom(drwMasterEntity)
-                .where(drwMasterEntity.drwNo.eq(1L))
-                .fetchOne();
-
-        assertThat(result.getDrwNo()).isEqualTo(1L);
-    }
 }
