67c81d4360432a1fd1e6115dac71caee1afd1d67
nimkoes <xxxelppa@gmail.com>
2023-03-15 12:48:43 +0900

    엔티티 생성 테스트

--- src/main/java/me/nimkoes/ltoplz/entity/DrwMasterEntity.java
+++ src/main/java/me/nimkoes/ltoplz/entity/DrwMasterEntity.java
@@ -1,13 +1,14 @@
 package me.nimkoes.ltoplz.entity;
 
-import lombok.Data;
+import lombok.Getter;
 import me.nimkoes.ltoplz.model.DrwModel;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.Objects;
 
-@Data
+@Getter
 @Entity
 @Table(name = "DRW_MASTER")
 public class DrwMasterEntity {
@@ -52,19 +53,44 @@ public class DrwMasterEntity {
     @Column(name = "BONUS_NO")
     private Integer bnusNo;
 
+    private DrwMasterEntity(Long id, Long drwNo, LocalDate issueDt, Integer drwtNo1, Integer drwtNo2, Integer drwtNo3, Integer drwtNo4, Integer drwtNo5, Integer drwtNo6, Integer bnusNo) {
+        this.id = id;
+        this.drwNo = drwNo;
+        this.issueDt = issueDt;
+        this.drwtNo1 = drwtNo1;
+        this.drwtNo2 = drwtNo2;
+        this.drwtNo3 = drwtNo3;
+        this.drwtNo4 = drwtNo4;
+        this.drwtNo5 = drwtNo5;
+        this.drwtNo6 = drwtNo6;
+        this.bnusNo = bnusNo;
+    }
+
     public static DrwMasterEntity byDrwModel(DrwModel drwModel) {
-        DrwMasterEntity entity = new DrwMasterEntity();
+        return new DrwMasterEntity(
+                null
+                , drwModel.getDrwNo()
+                , drwModel.getDrwNoDate()
+                , drwModel.getDrwtNo1()
+                , drwModel.getDrwtNo2()
+                , drwModel.getDrwtNo3()
+                , drwModel.getDrwtNo4()
+                , drwModel.getDrwtNo5()
+                , drwModel.getDrwtNo6()
+                , drwModel.getBnusNo()
+        );
+    }
 
-        entity.setDrwNo(drwModel.getDrwNo());
-        entity.setIssueDt(drwModel.getDrwNoDate());
-        entity.setDrwtNo1(drwModel.getDrwtNo1());
-        entity.setDrwtNo2(drwModel.getDrwtNo2());
-        entity.setDrwtNo3(drwModel.getDrwtNo3());
-        entity.setDrwtNo4(drwModel.getDrwtNo4());
-        entity.setDrwtNo5(drwModel.getDrwtNo5());
-        entity.setDrwtNo6(drwModel.getDrwtNo6());
-        entity.setBnusNo(drwModel.getBnusNo());
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DrwMasterEntity that = (DrwMasterEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(drwNo, that.drwNo) && Objects.equals(issueDt, that.issueDt) && Objects.equals(drwtNo1, that.drwtNo1) && Objects.equals(drwtNo2, that.drwtNo2) && Objects.equals(drwtNo3, that.drwtNo3) && Objects.equals(drwtNo4, that.drwtNo4) && Objects.equals(drwtNo5, that.drwtNo5) && Objects.equals(drwtNo6, that.drwtNo6) && Objects.equals(bnusNo, that.bnusNo);
+    }
 
-        return entity;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, drwNo, issueDt, drwtNo1, drwtNo2, drwtNo3, drwtNo4, drwtNo5, drwtNo6, bnusNo);
     }
 }
---
+++ src/test/java/me/nimkoes/ltoplz/entity/DrwMasterEntityTest.java
@@ -0,0 +1,25 @@
+package me.nimkoes.ltoplz.entity;
+
+import me.nimkoes.ltoplz.model.DrwModel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DrwMasterEntityTest {
+
+    @Test
+    @DisplayName("엔티티 생성 테스트")
+    public void 엔티티_생성_테스트() {
+        DrwModel drwModel = new DrwModel(
+                1L, LocalDate.now(),
+                12, 21, 34, 3, 5, 6, 27);
+
+        DrwMasterEntity entity_first = DrwMasterEntity.byDrwModel(drwModel);
+        DrwMasterEntity entity_second = DrwMasterEntity.byDrwModel(drwModel);
+
+        assertThat(entity_first).isEqualTo(entity_second);
+    }
+}
